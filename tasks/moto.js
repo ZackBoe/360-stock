c20d2f6af36392e967d637fe962d1adcff1f6a49
Zack Boehm <zacknut@gmail.com>
2014-09-14 23:23:10 -0400

    Add moto stock refresh task

---
+++ tasks/moto.js
@@ -0,0 +1,31 @@
+exports.task = {
+  name:          'moto',
+  description:   'Refreshes Motorola online availablity',
+  frequency:     60 * 1000,
+  queue:         'stock',
+  plugins:       [],
+  pluginOptions: {},
+
+  run: function(api, params, next){
+    var request = require('request');
+
+    request(api.config.keys.motorequest, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var cleaned = body.replace(api.config.keys.motoregex);
+        data = JSON.parse(cleaned);
+        api.log(data.variants[0].availability);
+        api.log(data.variants[1].availability);
+
+        if(data.variants[0].availability == 'NOT_AVAILABLE') api.cache.save('moto_black', false);
+        else api.cache.save('moto_black', true);
+
+        if(data.variants[1].availability == 'NOT_AVAILABLE') api.cache.save('moto_silver', false);
+        else api.cache.save('moto_silver', true);
+
+
+        next(true, null);
+      }
+      else { api.log(error); next(false, null); }
+      })
+  }
+};
